6b06733534233f6e3c2b6d1ad4ce74eb97c8188c
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-26 00:32:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,13 @@
 class shoe
 
   attr_accessor :color, :size, :material, :condition
-
+  attr_accessor :brand
+  
   def initialize(brand)
     @brand = brand
   end
 
   def cobble
     puts "Shoe has been repaired."
+  end
+end
