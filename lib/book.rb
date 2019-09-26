f60c9f57908a25b6af264fa16ab549c20d9bc2ea
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-26 00:15:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
 
+  def initialize(title = "And Then There Were None", author, pages, genre)
+    @title = title
+    @author = author
+    @pages = pages
+    @genre = genre
+  end
 
+  def title=(title = "And Then There Were None")
+    @title = title
+  end
+
+  def title
+    @title
+  end
